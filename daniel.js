e1e871558d6592a15c18a67120bedc52db3b5454
daniel luke <manthid5@gmail.com>
2026-04-23 10:57:03 +0300

    edit: change resuts to different order

--- daniel.js
+++ daniel.js
@@ -1,13 +1,13 @@
 let randomNum = Math.floor(Math.random() * 50) + 1;
 switch (true) {
-  case randomNum % 3 === 0 && randomNum % 5 === 0:
-    console.log("fizzbuzz");
-    break;
   case randomNum % 3 === 0:
     console.log("fizz");
     break;
   case randomNum % 5 === 0:
     console.log("buzz");
+    break;
+  case randomNum % 3 === 0 && randomNum & 5 ===0:
+    console.log("fizzbuzz");
     break;
   default:
     console.log("Null");
